ebdee6cec327e48235e6fa29f571347e84078689
clement-soler <clement.soler@hetic.net>
2016-12-13 22:50:42 +0100

    start app

---
+++ CocktailApp/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  CocktailApp
+//
+//  Created by Clement Soler on 13/12/2016.
+//  Copyright © 2016 Clement Soler. All rights reserved.
+//
+
+import Foundation
---
+++ CocktailApp/SignUpViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SignUpViewController.swift
+//  CocktailApp
+//
+//  Created by Clement Soler on 13/12/2016.
+//  Copyright © 2016 Clement Soler. All rights reserved.
+//
+
+import Foundation
